cb35a8c0bdcb1725303320d68d830a4d853e3c52
Nestor Gonzalez <nestor.ge222@gmail.com>
2020-04-06 12:49:13 -0700

    hello world

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse('Hello World')
+
+
 
 urlpatterns = [
+
+    path('hello-word/', hello_world),
     path('admin/', admin.site.urls),
 ]
